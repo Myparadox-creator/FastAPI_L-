b4ea9e43f56ad2a88caa6334bcff4d0280564ffd
Aditya R. Satapathy <adityaranjanwxd@gmail.com>
2026-03-17 09:34:55 +0530

    initial a commit

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World! FastAPI is awesome."}
+
+@app.get("/candidates/{candidate_id}")
+def get_candidate(candidate_id: int):
+    return {"candidate_id": candidate_id, "status": "i am alive"}
